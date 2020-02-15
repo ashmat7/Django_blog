b73e9898301baf03cc30417c46d4533491d9866d
ASHIESH MATHEWS <ashieshmathews@gmail.com>
2020-02-15 18:29:03 +0530

    Forms added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from .models import Post
+from .forms import PostForm
+
 
 def home(request):
     posts=Post.objects.all
     return render(request,'index.html',{'posts':posts})
+
+def createPostView(request):
+    if request.method == "POST":
+        form = PostForm(request.POST) #to get the data posted
+        if form.is_valid():
+            post = form.save(commit=False) #commit=false ensures the data is not saved in database but returned as a post object
+            post.author = request.user #sets the author to current logged in user
+            post.save() #saves the modified object having the author field as current logged in user
+            return redirect('home')
+
+    else:
+        form = PostForm()
+
+    return render(request, 'add.html', {'form': form})
+
+
